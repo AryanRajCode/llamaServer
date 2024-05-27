fd44ce2762fcb5895fa9aed88857d05016c108e8
Aryan Raj <168881081+AryanRajCode@users.noreply.github.com>
2024-05-27 13:05:40 +0530

    Update clientweb.py

--- clientweb.py
+++ clientweb.py
@@ -3,17 +3,14 @@ import socket
 
 app = Flask(__name__)
 
-# Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Get the local machine name and port
+
 host = "0.0.0.0"
 port = 12345
 
-# Connect to the server
 client_socket.connect((host, port))
 
-# Function to send messages to the server
 def send_message(message):
     client_socket.send(message.encode('utf-8'))
     response = client_socket.recv(1024).decode('utf-8')
@@ -30,7 +27,6 @@ def send_message_route():
         response = send_message(message)
         return response
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4040)
